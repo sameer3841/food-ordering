c2fd1d20602fc22286771006ed20a979d9211909
sameer3841 <81244405+sameer3841@users.noreply.github.com>
2024-02-20 09:12:34 -0500

    some progress

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,12 @@
 import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+// import EditScreenInfo from '@/src/components/EditScreenInfo';
+ import { Text, View } from '@/src/components/Themed';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={{fontSize:20, fontWeight:'bold'}}>Pizza</Text>
+      <Text>$12.99</Text>
     </View>
   );
 }
