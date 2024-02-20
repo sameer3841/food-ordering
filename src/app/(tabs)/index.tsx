db144496513c2134d9ffb5deef5c1fbb964516a8
sameer3841 <81244405+sameer3841@users.noreply.github.com>
2024-02-20 14:46:25 -0500

    made progress in index.tsx

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,29 +1,42 @@
-import { StyleSheet } from 'react-native';
-// import EditScreenInfo from '@/src/components/EditScreenInfo';
- import { Text, View } from '@/src/components/Themed';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Colors from '@/src/constants/Colors';
+import products from '@/assets/data/products';
+
+const product = products[1]; 
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={{fontSize:20, fontWeight:'bold'}}>Pizza</Text>
-      <Text>$12.99</Text>
+      <Image source={{uri: product.image}} style={styles.image}/>
+      <Text style={Object.assign({}, styles.title, styles.text)}>{product.name}</Text>
+      <Text style={Object.assign({}, styles.price, styles.text)}>${product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 30
+
+  },
+  image: {
+    width: '90%',
+    aspectRatio: 1,
+    marginTop: 20,
+    marginHorizontal: 20
   },
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize:18, 
+    fontWeight:'600',
+    marginVertical: 10
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold' 
   },
+  text:{
+    marginLeft: 10,
+  }
 });
